0002fc98430bec41008aaca17e0aaf15365fac66
Administrador <Rafo@MacBook-White.local>
2015-10-11 13:08:07 -0500

    modificar el footer y cambiar  el emerald

---
+++ js/main.js
@@ -0,0 +1,47 @@
+jQuery(function ($) {
+
+    $('.centered').each(function (e) {
+        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    });
+
+    $(window).resize(function () {
+        $('.centered').each(function (e) {
+            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+        });
+    });
+    
+//scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+            }, 1000);
+                    return false;
+      }
+    }
+  });
+});
+ {};
+
+    //contact form
+    var form = $('.contact-form');
+    form.submit(function () {
+        $this = $(this);
+        $.post($(this).attr('action'), function (data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    //goto top
+    $('.gototop').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+});
