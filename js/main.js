7489d8378b843a9974e3137e9c3db32b4c459aab
shirleybacilio <shirleybacilio@gmail.com>
2015-10-13 10:37:08 -0500

    efectos ready xD

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ $(function() {
 });
 
 
+jQuery(function ($) {
+
+    $('.centered').each(function (e) {
+        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    });
+
+    $(window).resize(function () {
+        $('.centered').each(function (e) {
+            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+        });
+    });
+
+
+     
